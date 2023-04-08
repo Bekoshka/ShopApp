c98363f21238592d6cac038e63b698a18882f8d9
Bekoshka <87566164+Bekoshka@users.noreply.github.com>
2023-04-08 13:21:25 +0300

    requirements.txt + куча бесполезных файлов которые не работают

--- core.py
+++ core.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
+from loginform import LoginForm
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Пользователь 1"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)

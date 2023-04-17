568d0eb69e70639e22026d60c7e21f945a7880ec
Vadim Salomatin <vadim.d.salomatin@gmail.com>
2023-04-17 23:01:58 +0300

    products

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 
 import db_session
-from handler import auth, main
+from handler import auth, main, productcategory, product
 from model.user import User
 
 app = Flask(__name__)
@@ -10,6 +10,8 @@ app.config.from_pyfile('config.py')
 
 app.register_blueprint(auth.blueprint)
 app.register_blueprint(main.blueprint)
+app.register_blueprint(product.blueprint)
+app.register_blueprint(productcategory.blueprint)
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
